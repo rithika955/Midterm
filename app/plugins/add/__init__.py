f41f27fcfd89cb175bb69c1e35d5e199b5460ed7
rithika955 <rk955@njit.edu>
2024-10-25 19:55:07 -0400

    fixes

--- app/plugins/add/__init__.py
+++ app/plugins/add/__init__.py
@@ -7,12 +7,12 @@ class AddCommand(Command):
     def execute(self):
         try:
             #hist_inst = History()
-            input1 = float(input("Enter the first number: "))
-            input2 = float(input("Enter the second number: "))
+            input1 = float(input("Please enter the first number: "))
+            input2 = float(input("Please enter the second number: "))
             result = input1 + input2
             print('The result of the operations is', result)
             logging.info(f'addition of {input1} and {input2} = {input1 + input2}')
-            logging.info('The Addition operation was performed successfully')
+            logging.info('Addition operation successfully performed')
             data = ['add', input1, input2]
             #existing_data = hist_inst.get_as_list()
             #existing_data.append(data)
